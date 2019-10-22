b86a4e2cce7ddf7d594b4784a1e77c1d49821e50
Adrià Abella <adria.abella@visyon360.com>
2019-10-22 09:02:58 +0200

    Hello world! Xcode project added.

---
+++ BrewMock/BrewMock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrewMock
+//
+//  Created by Josep Lorente on 22/10/2019.
+//  Copyright © 2019 AdriaAbella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
